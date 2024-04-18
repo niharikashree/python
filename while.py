34bb0a4f39465e40f53384f30454ff920616617b
niharikashree <167109338+niharikashree@users.noreply.github.com>
2024-04-18 21:25:19 +0530

    while loop

---
+++ while.py
@@ -0,0 +1,38 @@
+#WHILE LOOP BASIC 
+print("hi")
+i=25
+while i<28:
+    print("statement 1")
+    print("statement 2")
+    i=i+1
+print("hello end of while")
+
+#EXAMPLES FOR WHILE LOOP
+i=56
+while i<=97:
+    print(i)
+    i=i+1
+
+j=1222
+while j>=995:
+    print(j)
+    j=j-1
+
+k=1
+while k<=10:
+    res=5*k
+    print("5*",k,"=",res)
+    k=k+1
+
+n=10
+while n>=1:
+    res=7*n
+    print("7*",n,"=",res)
+    n=n-1
+
+s=12
+while s<=56:
+    t=s
+    if t%2==0:
+        print(s)
+    s=s+1
