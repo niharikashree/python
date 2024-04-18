6e48f6a7b6326e134c60f428796be889fd13b0cc
niharikashree <167109338+niharikashree@users.noreply.github.com>
2024-04-18 20:53:28 +0530

    basic list operations

---
+++ list_basic.py
@@ -0,0 +1,39 @@
+LISTS BASIC FUNCTIONS
+
+
+l=list(map(int, input().split()))
+print(l)
+w=set(l)
+print(w)
+v=list(w)
+print(v)
+print(l[-1])
+print(l[-2])
+
+word =input()
+print(word)
+# lists are immutable
+# but we use slicing to handle lists
+t=word[5:10]
+print(t)
+q=word[14:20]
+print(q)
+r=word[21:26]
+print(r)
+u=word[27:30]
+print(u)
+d=word[5:16] 
+print(d)
+g=word[0:4]
+print(g)
+h=word[17:30]
+print(h)
+j=word[9:13]
+print(j)
+
+a=word[14:20]
+print(a[::-1])
+b=word[5:13]
+print(b[::-1])
+
+
