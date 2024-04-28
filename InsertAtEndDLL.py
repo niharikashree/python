7aaadc8ba121135bd461f61aa7415c29376d2945
niharikashree <167109338+niharikashree@users.noreply.github.com>
2024-04-28 14:30:06 +0530

    insertion at end of DLL

---
+++ InsertAtEndDLL.py
@@ -0,0 +1,50 @@
+#INSERTION AT END OF DLL
+class Box:
+    def __init__(self,data):
+        self.data=data
+        self.prev=None
+        self.next=None
+def findtail(head):
+    if head==None:
+        return None
+    tail=head
+    while tail.next!=None:
+        tail=tail.next
+    return tail
+def insertatend(head,ele):
+    newnode=Box(ele)
+    if head==None:
+        return newnode
+    tail=findtail(head)
+    tail.next=newnode
+    newnode.prev=tail
+    return head
+
+def printlefttoright(head):
+    curr=head
+    while curr!=None:
+        print(curr.data,end=" ")
+        curr=curr.next
+    print()
+    
+    
+def printrighttoleft(head):
+    tail=findtail(head)
+    while tail!=None:
+        print(tail.data,end=" ")
+        tail=tail.prev
+    print()
+    
+n=int(input())
+l=list(map(int,input().split()))
+newelement=int(input())
+head=None
+for ele in l:
+    head=insertatend(head,ele)
+printlefttoright(head)
+printrighttoleft(head)
+
+head=insertatend(head,newelement)
+
+printlefttoright(head)
+printrighttoleft(head)
